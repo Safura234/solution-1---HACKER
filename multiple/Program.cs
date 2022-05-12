19949fc22476dcee4ceb751a29fd85b4ac7f5efc
Safura234 <safuramg@code.edu.az>
2022-05-12 10:11:27 +0400

    https://github.com/Safura234/solution-1---HACKER.git

---
+++ multiple/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace multiple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("Enter your number,please");
+            int i = Convert.ToInt16(Console.ReadLine());
+            for ( i = 0; i <= 100; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                    break;
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                    break;
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("%d/t", i);
+                }
+                }
+                }
+            }
+        }
+ 
